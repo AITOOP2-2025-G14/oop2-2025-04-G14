4298ee296b886f7650074a20b610e81d91f92478
N.Takuya <nishiinfo0408@gmail.com>
2025-10-23 15:29:40 +0900

    作業２

--- ffmpeg_record.py
+++ ffmpeg_record.py
@@ -22,7 +22,7 @@ try:
     # -i <入力デバイス名>: デバイス名を指定
     (
         ffmpeg
-        .input(':2', format='avfoundation', t=duration) # macOSの例
+        .input(':0', format='avfoundation', t=duration) # macOSの例
         .output(output_file, acodec='pcm_s16le', ar='44100', ac=1)
         .run(overwrite_output=True)
     )
@@ -32,3 +32,39 @@ except ffmpeg.Error as e:
     print(f"エラーが発生しました: {e.stderr.decode()}")
 except Exception as e:
     print(f"予期せぬエラー: {e}")
+
+import mlx_whisper
+from pydub import AudioSegment
+import numpy as np
+
+# 音声ファイルを指定して文字起こし
+audio_file_path = "python-audio-output.wav"
+
+result = mlx_whisper.transcribe(
+  audio_file_path, path_or_hf_repo="mlx-community/whisper-base-mlx"
+)
+print(result)
+
+# 音声データを指定して文字起こし
+def preprocess_audio(sound):
+    if sound.frame_rate != 16000:
+        sound = sound.set_frame_rate(16000)
+    if sound.sample_width != 2:
+        sound = sound.set_sample_width(2)
+    if sound.channels != 1:
+        sound = sound.set_channels(1)
+    return sound
+
+audio_data = []
+
+# 音声データを音声ファイルから読み取る
+audio_data.append(AudioSegment.from_file("python-audio-output.wav", format="wav"))
+
+for data in audio_data:
+    sound = preprocess_audio(data)
+    # Metal(GPU)が扱えるNumpy Array形式に変換
+    arr = np.array(sound.get_array_of_samples()).astype(np.float32) / 32768.0
+    result = mlx_whisper.transcribe(
+        arr, path_or_hf_repo="mlx-community/whisper-base-mlx"
+    )
+    print(result)
